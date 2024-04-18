b7b363f6fa877ee63bd80cd92e277ad14d35c63f
Sourabh <ghorpadesourabh2001@gmail.com>
2024-04-18 18:36:49 +0530

    Assignment Que3

--- Assignment/Que3.py
+++ Assignment/Que3.py
@@ -8,11 +8,51 @@ connection = mysql.connector.connect(
     database = "iotdb"
 )
 
-query = "select * from Employee;"
+#query = "select * from Employee order by Salary DESC;"" select Name from Employee;"" select * from Employee; "
+
+#cursor = connection.cursor()
+
+#cursor.execute(query)
+
+#records = cursor.fetchall()     
+
+#print(records)
+
+#cursor.close()
+
+#connection.close()
+
+query1 = "select * from Employee order by Salary DESC;"
+query2 = " select Name from Employee;"
+query3 = " select Department from Employee;"
+query4 = "select * from Employee order by Salary ASC;"
 
 cursor = connection.cursor()
 
-cursor.execute(query)
+temp = input("Enter your choice : ")
+if(temp == "high"):
+    
+    cursor.execute(query1)
+
+    records = cursor.fetchall()     
+
+    print(records)
+
+elif(temp == "low"):
+
+    cursor.execute(query4)
+
+    records = cursor.fetchall()     
+
+    print(records)
+
+cursor.execute(query2)
+
+records = cursor.fetchall()     
+
+print(records)
+
+cursor.execute(query3)
 
 records = cursor.fetchall()     
 
